6166f95f5d993357db93dcf4a72d00d5f64d8851
tarek-salamaa <tarek-salamaa>
2023-02-22 11:41:24 +0200

    String Method

--- Js/main.js
+++ Js/main.js
@@ -1,54 +1,16 @@
-/*
-  String Methods
-  - substring(Start [Mand], End [Opt] Not Including End)
-  --- Start > End Will Swap
-  --- Start < 0 It Start From 0
-  --- Use Length To Get Last Character
-  - substr(Start [Mand], Characters To Extract)
-  --- Start >= Length = ""
-  --- Negative Start From End
-  - includes(Value [Mand], Start [Opt] Default 0) [ES6]
-  - startsWith(Value [Mand], Start [Opt] Default 0) [ES6]
-  - endsWith(Value [Mand], Length [Opt] Default Full Length) [ES6]
-*/
+let myName = "  Tarek  ";
 
-// let a = "Elzero Web School";
+console.log(myName[0]);
+console.log(myName[5]);
 
-// string.substring(startIndex, endIndex);
+console.log(myName.charAt(0));
+console.log(myName.charAt(5));
 
-// console.log(a.substring(2, 6));
-// console.log(a.substring(6, 2));
+console.log(myName.length);
 
-// let str = "Hello World";
-// let newStr = str.substring(6); // newStr = "World"
+console.log(myName.trim());
 
-// console.log(newStr);
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
 
-// let str = "Hello World";
-// let newStr = str.substring(0, 5); // newStr = "Hello"
-
-// console.log(newStr);
-
-// let str = "Hello World";
-// let newStr = str.substring(3, 8); // lo Wo
-
-// console.log(newStr);
-
-// str.substr(start, length)
-
-// const str = "Hello, World!";
-
-// console.log(str.substr(7)); // "World!"
-// console.log(str.substr(7, 3)); // "Wor"
-// console.log(str.substr(-6)); // "World!"
-// console.log(str.substr(-6, 4)); // "Worl"
-// console.log(str.substr(0, 5)); // Hello
-
-// var str = "Hello, World!";
-// var result = str.substr(7, 5);
-// console.log(result); // outPut in console "World"
-
-const myName = "Tarek";
-const myAge = 17.5;
-
-console.log(Math.pow(myAge));
+console.log(myName.trim().charAt(4).toUpperCase());
